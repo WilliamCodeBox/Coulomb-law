3e0c8e11236abd19a2f21e029f504104671e2f4d
Liu Chang <luroylau@163.com>
2020-05-06 14:02:00 +0800

    add typing hint

--- vector.py
+++ vector.py
@@ -1,34 +1,37 @@
 import numpy as np
+from typing import TypeVar, Union, NoReturn
+
+T = Union[int, float]
 
 
 class Vector(object):
-    def __init__(self, x, y, z):
+    def __init__(self, x: T, y: T, z: T):
         self._x = x
         self._y = y
         self._z = z
 
     @property
-    def x(self):
+    def x(self) -> T:
         return self._x
 
     @x.setter
-    def x(self, x):
+    def x(self, x: T):
         self._x = x
 
     @property
-    def y(self):
+    def y(self) -> T:
         return self._y
 
     @y.setter
-    def y(self, y):
+    def y(self, y: T):
         self._y = y
 
     @property
-    def z(self):
+    def z(self) -> T:
         return self._z
 
     @z.setter
-    def z(self, z):
+    def z(self, z: T):
         self._z = z
 
     def __sub__(self, other):
@@ -38,19 +41,19 @@ class Vector(object):
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
 
-    def __mul__(self, a):
+    def __mul__(self, a: T):
         return Vector(self.x * a, self.y * a, self.z * a)
 
-    def __rmul__(self, a):
+    def __rmul__(self, a: T):
         return Vector(a * self.x, a * self.y, a * self.z)
 
-    def __truediv__(self, a):
+    def __truediv__(self, a: T):
         return Vector(self.x / a, self.y / a, self.z / a)
 
-    def norm(self):
+    def norm(self) -> T:
         return np.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.x, self.y, self.z}"
 
 
@@ -59,12 +62,12 @@ if __name__ == "__main__":
     print(a)
     a = a * 3
     print(a)
-    a = 3 * a
-    print(a)
+    d = a * a
+    print(d)
     b = Vector(1, 0, 0)
     c = a - b
     print(c)
-    dis = c.norm()
+    dis: T = c.norm()
     print(dis)
 
     unit = c / dis
