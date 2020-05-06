0fd3f0f5002e22388db576a088236749b3eff804
Liu Chang <luroylau@163.com>
2020-05-06 14:13:42 +0800

    superposition principle for multiple charges

--- coulomb.py
+++ coulomb.py
@@ -44,4 +44,49 @@ where R = abs(r2 - r1)
 def vectorizedCoulomb(q1, v1, q2, v2):
     dis = (v2 - v1).norm()
     force = q1 * q2 / (4.0 * np.pi * eps0 * np.pow(dis, 2))
-    return force, (v2 - v1) / dis
+    uniVec = (v2 - v1) / dis
+    result = force * uniVec
+    return result, force, uniVec
+
+
+""" 
+
+Principle of Superposition
+
+The resultant force F on a charge Q located at point vector r is 
+the vector sum of the forces exerted on Q by each of the charges Q1, Q2, ..., QN,
+
+"""
+
+from vector import Vector
+
+
+def multiCharges(q, v, Q, V):
+    N = len(Q)
+    assert (N == len(V))
+
+    factor = q / (4.0 * np.pi * eps0)
+
+    sum = Vector(0.0, 0.0, 0.0)
+
+    for idx in np.arange(0, N):
+        dis = (v - V[idx]).norm()
+        sum = sum + ((v - V[idx]) / np.power(dis, 3)) * Q[idx]
+    result = sum * factor
+    force = result.norm()
+    uniVec = result / result.norm()
+    return result, force, uniVec
+
+
+if __name__ == "__main__":
+    q1 = 1e-3
+    v1 = Vector(3, 2, -1)
+
+    q2 = -2e-3
+    v2 = Vector(-1, -1, 4)
+
+    q = 10e-9
+    v = Vector(0, 3, 1)
+
+    res, force, uniVec = multiCharges(q, v, [q1, q2], [v1, v2])
+    print(res * 1e3, force, uniVec)
